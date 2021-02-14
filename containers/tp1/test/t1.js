fa6c8cb481933535c87fa26b40e0d8f885165fd1
le99 <le.florez602@uniandes.edu.co>
2021-02-14 11:03:05 -0500

    Configured mocha

---
+++ containers/tp1/test/t1.js
@@ -0,0 +1,14 @@
+//https://mochajs.org
+//https://www.chaijs.com/guide/styles/#assert
+const assert = require('chai').assert;
+
+
+describe('works', ()=>{
+  it('works', ()=>{
+    assert.equal('a', 'a')
+  })
+
+  it('works2', ()=>{
+
+  })
+});
