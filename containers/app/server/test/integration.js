c10f78c3ebd2d0e52a4b31ac173f373cd9a0947f
le99 <le.florez602@uniandes.edu.co>
2021-03-12 09:25:12 -0500

    Test configuration

---
+++ containers/app/server/test/integration.js
@@ -0,0 +1,10 @@
+//https://mochajs.org
+//https://www.chaijs.com/guide/styles/#assert
+const assert = require('chai').assert;
+
+describe('simple', ()=>{
+  it('constants are defined', ()=>{
+    assert.equal('1', '1');
+  })
+
+});
