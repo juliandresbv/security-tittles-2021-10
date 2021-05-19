c723c2a85dc9ecba19f1e8bb7fd58f60e0603fe2
afrodriguezm1 <af.rodriguezm1@uniandes.edu.co>
2021-05-19 17:47:52 -0500

    Quitar el app distributed

--- containers/app/ledger-distributed-api/server.js
+++
@@ -1,9 +0,0 @@
-const { app } = require('./app');
-const dotenv = require('dotenv'); dotenv.config();
-
-const port = process.env.PORT || 5110;
-
-(async function run() {
-  console.log(`Running ledger-distributed-api in port ${port}...`);
-  await app.listen(port);
-})();
